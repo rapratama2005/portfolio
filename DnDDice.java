8d933dec474aa19033f81249edbe401654e49408
I'mNotRisky <rapratama2005@gmail.com>
2023-01-12 08:33:36 -0600

    a

---
+++ DnDDice.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*; 
+import java.lang.*;
+
+
+public class DnDDice {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ad = Advantage, Di = Disadvantage, 4d6 = 4 Six Sided Dice");
+        System.out.println("Enter in Dice");
+        
+    }
+}
+
